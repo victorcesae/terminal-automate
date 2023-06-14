e4ee7a8487aa147437c1f7472de8764be712c366
Victor Evangelista <auxiliar.ti@amrabrasil.com.br>
2023-06-14 18:01:14 -0300

    fix: enable edit, add or remove commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,19 @@
 import * as vscode from "vscode";
+import { v4 as uuidv4 } from "uuid";
 interface sequence {
   text: string;
   terminal: string;
+  id: string;
 }
 interface CommandConfig {
   command: string;
-  sequence: sequence[];
+  sequences: sequence[];
+  id: string;
 }
 
 const extension = "terminal-automate";
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   const disposables: vscode.Disposable[] = [];
   const dynamicCommand = vscode.commands.registerCommand(
     `${extension}.dynamicCommand`,
@@ -19,11 +22,11 @@ export function activate(context: vscode.ExtensionContext) {
       const commands: CommandConfig[] = config.get("commands") || [];
       const selectedCommand = await vscode.window.showQuickPick(
         getAvailableCommands(commands),
-        { placeHolder: "Selecione um comando" }
+        { placeHolder: "Select a command" }
       );
       if (selectedCommand) {
         const command = commands.find(
-          (cmd) => cmd.command === selectedCommand.label
+          (cmd) => cmd.id === selectedCommand.description
         );
         if (command) {
           executeCommand(command);
@@ -37,22 +40,32 @@ export function activate(context: vscode.ExtensionContext) {
     async () => {
       const config = vscode.workspace.getConfiguration(extension);
       const commands: CommandConfig[] = config.get("commands") || [];
-      const commandName = await vscode.window.showInputBox({
-        prompt: "Digite o nome da ação",
-      });
-      if (commandName) {
-        const sequence = await openSequenceForm();
-        if (sequence) {
-          const newCommand: CommandConfig = {
-            command: commandName,
-            sequence: sequence,
-          };
-          commands.push(newCommand);
+      const quickItems = getAvailableCommands(commands);
 
-          await saveCommandsToSettings(commands, extension);
-          vscode.window.showInformationMessage(
-            `Comando "${commandName}" adicionado com sucesso.`
-          );
+      const selectedCommand = await vscode.window.showQuickPick(
+        [
+          { label: "Add or Edit a command", description: "addOrEdit" },
+          ...(commands && commands.length > 0
+            ? [
+                {
+                  label: "Delete a command",
+                  description: "delete",
+                },
+              ]
+            : []),
+        ],
+        { placeHolder: "Select a operation type" }
+      );
+      if (selectedCommand) {
+        switch (selectedCommand.description) {
+          case "addOrEdit":
+            await addOrEditCommand(commands, quickItems);
+            break;
+          case "delete":
+            await removeCommand(commands, quickItems);
+            break;
+          default:
+            break;
         }
       }
     }
@@ -64,7 +77,6 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(...disposables);
 
-  // Prompt the user to register a sequence after installation
   vscode.window
     .showInformationMessage(
       "Welcome to Terminal Automate! Do you want to register a sequence?",
@@ -76,6 +88,7 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.commands.executeCommand(`${extension}.openForm`);
       }
     });
+  // Prompt the user to register a sequence after installation
 }
 
 function getAvailableCommands(
@@ -83,18 +96,22 @@ function getAvailableCommands(
 ): vscode.QuickPickItem[] {
   return commands.map((cmd) => ({
     label: cmd.command,
+    description: cmd.id,
   }));
 }
 
 async function executeCommand(command: CommandConfig): Promise<void> {
-  const { sequence, command: commandName } = command;
+  const { sequences, command: commandName } = command;
 
   vscode.window.showInformationMessage(
-    `Executando comando no terminal "${commandName}"!`
+    `Executing command in terminal "${commandName}" !`
   );
-  for (const step of sequence) {
-    if (step.hasOwnProperty("terminal") && step.hasOwnProperty("text")) {
-      const terminalName = step.terminal;
+  for (const sequence of sequences) {
+    if (
+      sequence.hasOwnProperty("terminal") &&
+      sequence.hasOwnProperty("text")
+    ) {
+      const terminalName = sequence.terminal;
       let terminal = vscode.window.terminals.find(
         (term) => term.name === terminalName
       );
@@ -104,34 +121,41 @@ async function executeCommand(command: CommandConfig): Promise<void> {
       }
 
       // Envia o texto para o terminal usando o comando 'workbench.action.terminal.sendSequence'
-      terminal.sendText(step.text);
+      terminal.sendText(sequence.text);
     } else {
-      vscode.window.showErrorMessage(`Passo inválido na sequência do comando.`);
+      vscode.window.showErrorMessage(`Invalid sequence in command sequence.`);
     }
   }
-  vscode.window.showInformationMessage(`Comando "${commandName}" executado!`);
+  vscode.window.showInformationMessage(`Command "${commandName}" executed!`);
 }
 
-async function openSequenceForm(): Promise<sequence[] | undefined> {
+async function openSequenceForm(
+  command?: CommandConfig
+): Promise<sequence[] | undefined> {
   const steps: sequence[] = [];
   let terminal = -1;
   while (true) {
     terminal++;
     const stepText = await vscode.window.showInputBox({
-      prompt: "Digite o comando para o próximo passo",
+      prompt: "Enter the command for the next step",
+      value: command?.sequences?.[terminal]?.text,
     });
     if (!stepText) {
+      if (command?.sequences?.[terminal])
+        command?.sequences?.splice(terminal, 1);
       break;
     }
 
     const stepTerminal = await vscode.window.showInputBox({
       prompt:
-        "Digite o nome do terminal (deixe em branco para usar o nome default)",
+        "Enter the name of the terminal (leave blank to use the default name)",
+      value: command?.sequences[terminal]?.terminal,
     });
 
     const step = {
       text: stepText,
-      terminal: stepTerminal ?? `${combineFirstTwoWords(stepText)}`,
+      terminal: stepTerminal ?? `${combineFirstTwoWords(stepText)}-${terminal}`,
+      id: command?.sequences[terminal]?.id ?? uuidv4(),
     };
 
     steps.push(step);
@@ -158,6 +182,99 @@ function combineFirstTwoWords(str: string): string {
     return words[0] + "-" + words[1];
   } else {
     return words[0];
+  }
+}
+
+async function addOrEditCommand(
+  commands: CommandConfig[],
+  quickItems: vscode.QuickPickItem[]
+) {
+  const selectedCommand = await vscode.window.showQuickPick(
+    [...[{ label: "Add new command", description: "new" }], ...quickItems],
+    {
+      placeHolder: "Select a command to edit or create a new one",
+    }
+  );
+  if (selectedCommand) {
+    const isNewCommand = selectedCommand.description === "new";
+    const command = isNewCommand
+      ? undefined
+      : commands.find((cmd) => cmd.id === selectedCommand.description);
+
+    let commandName = isNewCommand ? undefined : selectedCommand.label;
+    commandName = await vscode.window.showInputBox({
+      prompt: "Enter the action name",
+      value: command?.command,
+    });
+    if (!commandName) return;
+    if (commandName === "Add new command") {
+      vscode.window.showErrorMessage(`Invalid command name (reserved name).`);
+      return;
+    }
+    const sequences = await openSequenceForm(
+      isNewCommand ? undefined : command
+    );
+    if (!sequences) {
+      if (isNewCommand) {
+        commands?.splice(
+          commands.findIndex((cmd) => cmd.id === command?.id),
+          1
+        );
+      }
+      return;
+    }
+    const newCommand: CommandConfig = {
+      command: commandName,
+      sequences: sequences,
+      id: command?.id ?? uuidv4(),
+    };
+    if (isNewCommand) {
+      commands.push(newCommand);
+    } else {
+      commands.map((cmd) => {
+        if (cmd.id === newCommand.id) {
+          cmd.command = newCommand.command;
+          newCommand.sequences.map((newSequence) => {
+            const oldSeqIndex = cmd.sequences.findIndex(
+              (sqc) => sqc.id === newSequence.id
+            );
+            if (oldSeqIndex > -1) {
+              cmd.sequences[oldSeqIndex] = newSequence;
+            } else {
+              cmd.sequences.push(newSequence);
+            }
+          });
+        }
+      });
+    }
+
+    await saveCommandsToSettings(commands, extension);
+    vscode.window.showInformationMessage(
+      `Command "${commandName}" successfully added.`
+    );
+  }
+}
+async function removeCommand(
+  commands: CommandConfig[],
+  quickItems: vscode.QuickPickItem[]
+) {
+  const selectedCommand = await vscode.window.showQuickPick(quickItems, {
+    placeHolder: "Select a command to delete",
+  });
+  if (selectedCommand) {
+    const index = commands.findIndex(
+      (cmd) => cmd.id === selectedCommand.description
+    );
+    const command = commands[index];
+    commands.splice(
+      commands.findIndex((cmd) => cmd.id === selectedCommand.description),
+      1
+    );
+
+    await saveCommandsToSettings(commands, extension);
+    vscode.window.showInformationMessage(
+      `Command "${command.command}" successfully deleted.`
+    );
   }
 }
 export function deactivate() {}
